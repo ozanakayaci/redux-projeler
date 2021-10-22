1b1fe4fdf8e5c75b6a82a222943ece84b116c2c8
OzanAliKayacı <ozanalikayaci@gmail.com>
2021-10-22 18:09:33 +0300

    redux store and slice prepared

---
+++ text-generator/src/redux/textSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const textSlice = createSlice({
+  name: "text",
+  initialState: {},
+
+  reducers: {},
+});
+
+export const {} = textSlice.actions;
+export default textSlice.reducer;
