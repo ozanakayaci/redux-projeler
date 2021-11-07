db88079cffb1cc35f1261c09b821453ada6c508f
OzanAliKayacı <ozanalikayaci@gmail.com>
2021-11-07 15:09:15 +0300

    redux store,slice

---
+++ markdown-previewer/src/redux/textSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const textSlice = createSlice({
+  name: "text",
+  initialState: {},
+
+  reducers: {},
+});
+
+export default textSlice.reducer;
